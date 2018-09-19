c641d7f91275e95916b395b16d94f1836ea7994a
Christoph Thelen <doc_bacardi@users.sourceforge.net>
2018-09-19 09:05:59 +0200

    Add the Windows DLL to the artifact.

--- openocd/installer/install_windows.lua
+++ openocd/installer/install_windows.lua
@@ -6,7 +6,7 @@ if strDistId=='@JONCHKI_PLATFORM_DIST_ID@' and strCpuArch=='@JONCHKI_PLATFORM_CP
   t:install_dev('include',     '${install_dev_include}/')
   t:install_dev('lib',         '${install_dev_lib}/')
   t:install_dev('cmake',       '${install_dev_cmake}/')
-  t:install('lib/openocd.dll',  '${install_lua_cpath}/openocd/')
+  t:install('bin/openocd.dll',  '${install_lua_cpath}/openocd/')
   tResult = true
 end
 
